fbb31725c95613b50345140c3734375f56d3abed
Jack <jack_maskell@hotmail.co.uk>
2025-03-17 21:51:08 +0000

    Update 3 years to 4

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -16,7 +16,7 @@
         I was studying physics at university when I had my first proper go at programming. My first language was <a href="https://www.python.org/" target="_blank">Python</a>. 
         It scratched an itch that physics didn't. I could write some code, click a button, and see the fruits of my labour immediately. I loved it.<br/><br/>
     
-        I decided to switch degrees and study <a href="https://www.sheffield.ac.uk/undergraduate/courses/2025/computer-science-artificial-intelligence-bsc" target="_blank"> Artificial Intelligence and Computer Science</a> and while studying, I discovered web development. It perfectly combined my passion for programming and my many years of interest in graphic design. After graduating, I started looking for jobs in web development and landed one at Darktrace. For the past three years, I've been working as a full-stack engineer on the company's CRM, planning, designing, and coding new systems to support the business's sales operations. <br/><br/>
+        I decided to switch degrees and study <a href="https://www.sheffield.ac.uk/undergraduate/courses/2025/computer-science-artificial-intelligence-bsc" target="_blank"> Artificial Intelligence and Computer Science</a> and while studying, I discovered web development. It perfectly combined my passion for programming and my many years of interest in graphic design. After graduating, I started looking for jobs in web development and landed one at Darktrace. For the past four years, I've been working as a full-stack engineer on the company's CRM, planning, designing, and coding new systems to support the business's sales operations. <br/><br/>
     </div>
 
 </template>
